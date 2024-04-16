4d6d9f84658e204073285c1752714d97f8ae4cab
mohamed samir <mohamed_elshirbiny@hotmail.com>
2024-04-16 19:21:02 +0200

    task-8

--- models/user.py
+++ models/user.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import String, Column, Integer, ForeignKey, Float
+from sqlalchemy.orm import relationship
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     email = ''
     password = ''
     first_name = ''
     last_name = ''
+    places = relationship("Place", cascade="all, delete", backref="user")
