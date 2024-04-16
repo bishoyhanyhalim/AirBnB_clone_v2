5513c528ea4ff1cda5ba773f63dba2fdd1129269
bishoyhanyhalim <bishoyhanyhalim@gmail.com>
2024-04-16 14:11:29 -0700

    task 7 ok

--- models/user.py
+++ models/user.py
@@ -2,7 +2,7 @@
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import String, Column, Integer
-# from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
