d66b4b45fd714a7edb2714fd96ddba798e70906f
mohamed samir <mohamed_elshirbiny@hotmail.com>
2024-05-04 02:37:41 +0300

    task-2.7

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,46 +1,39 @@
 #!/usr/bin/python3
 """
-Fabric script to deploy tgz archive
-fab -f 2-do_deploy_web_static.py do_deploy:archive_path=filepath
-    -i private-key -u user
+A Fabric script  that distributes
+an archive to web servers
 """
 
+from fabric.api import local, run, env, put
 from os.path import exists
-from fabric.api import put, run, env
 
-env.hosts = ['35.243.128.200', '3.239.120.96']
+env.hosts = ['18.208.120.216', '100.25.110.24']
 
 
 def do_deploy(archive_path):
     """
-    copies archive file from local to my webservers
+    Distributes an archive to web servers
     """
-
     if not exists(archive_path):
         return False
+
     try:
-        file_name = archive_path.split("/")[-1].split(".")[0]
-        put(archive_path, "/tmp/")
+        archive = archive_path.split('/')[-1]
+        folder = archive.split('.')[0]
+        deploy_path = "/data/web_static/releases/"
+        tmp_path = "/tmp/"
 
-        run("mkdir -p /data/web_static/releases/{}".format(file_name))
-
-        run("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/"
-            .format(file_name, file_name))
-
-        run('rm -rf /tmp/{}.tgz'.format(file_name))
-
-        run(('mv /data/web_static/releases/{}/web_static/* ' +
-            '/data/web_static/releases/{}/')
-            .format(file_name, file_name))
-
-        run('rm -rf /data/web_static/releases/{}/web_static'
-            .format(file_name))
-
-        run('rm -rf /data/web_static/current')
-
-        run(('ln -s /data/web_static/releases/{}/' +
-            ' /data/web_static/current')
-            .format(file_name))
+        put_res = put(archive_path, tmp_path)
+        if put_res.failed:
+            return False
+        run(f"mkdir -p {deploy_path}{folder}")
+        run(f"tar -xzf {tmp_path}{archive} -C {deploy_path}{folder}")
+        run(f"rm {tmp_path}{archive}")
+        run(f"mv {deploy_path}{folder}/web_static/* {deploy_path}{folder}")
+        run(f"rm -rf {deploy_path}{folder}/web_static")
+        run(f"rm -rf /data/web_static/current")
+        run(f"ln -s {deploy_path}{folder}/ /data/web_static/current")
+        print("New version deployed!")
         return True
     except Exception:
         return False
