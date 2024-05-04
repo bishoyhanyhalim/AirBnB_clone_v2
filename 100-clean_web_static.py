2611aef872737aec10c069ebdb4396d6a3ea06c8
mohamed samir <mohamed_elshirbiny@hotmail.com>
2024-05-04 09:27:38 +0300

    task-100.3

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -21,14 +21,13 @@ def do_clean(number=0):
 
     with cd('/data/web_static/releases/'):
         releases = sorted(run('ls -tr').split())
-        print('releases:', releases)
+        releases = [r for r in releases if r.startswith('web_static_')]
         deletes = releases[:-number]
         for release in releases:
-            if release in deletes and release.startswith('web_static_'):
+            if release in deletes:
                 run(f"sudo rm -rf {release}")
 
     local_archs = sorted(os.listdir('versions'))
-    print("Local Archives:", local_archs)
     deletes = local_archs[:-number]
     for arch in local_archs:
         if arch in deletes and arch.startswith('web_static_'):
