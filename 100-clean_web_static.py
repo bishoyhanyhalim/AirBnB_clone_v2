dffb68aa79ff9ee05f667fb07957b83bca18f432
mohamed samir <mohamed_elshirbiny@hotmail.com>
2024-05-04 09:21:43 +0300

    task-100.1

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -6,22 +6,30 @@ Deletes out-of-date archives
 import os
 from fabric.api import *
 
-env.hosts = ['18.208.120.216', '100.25.110.24']
+env.hosts = ['52.87.230.55', '100.25.150.51']
+env.user = 'ubuntu'
+env.key_filename = '~/.ssh/school'
 
 
 def do_clean(number=0):
     """
     Delete out-of-date archives.
     """
-    number = 1 if int(number) == 0 else int(number)
+    number = int(number)
+    if number == 0:
+        number = 1
 
-    archives = sorted(os.listdir("versions"))
-    [archives.pop() for i in range(number)]
-    with lcd("versions"):
-        [local("rm ./{}".format(a)) for a in archives]
+    with cd('/data/web_static/releases/'):
+        releases = sorted(run('ls -tr').split())
+        print('releases:', releases)
+        deletes = releases[:-number]
+        for release in releases:
+            if release in deletes and release.startswith('web_static_'):
+                run(f"sudo rm -rf {release}")
 
-    with cd("/data/web_static/releases"):
-        archives = run("ls -tr").split()
-        archives = [a for a in archives if "web_static_" in a]
-        [archives.pop() for i in range(number)]
-        [run("rm -rf ./{}".format(a)) for a in archives]
+    local_archs = sorted(os.listdir('versions'))
+    print("Local Archives:", local_archs)
+    deletes = local_archs[:-number]
+    for arch in local_archs:
+        if arch in deletes and arch.startswith('web_static_'):
+            local(f"sudo rm -rf versions/{arch}")
